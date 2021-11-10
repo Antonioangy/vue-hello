72fb49942daf28b969493c0455298d62e20043b7
Antonioangy <dumitruangy26@gmail.com>
2021-11-10 16:18:26 +0100

    js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+
+    el: '#firstTime',
+    data: {
+        message: 'La mia prima volta su Vuejs',    
+        immagine: 'https://ardalis.com/static/4fb4ac75fbd03f00f42e4ac3586c8265/c3638/NotSureIfGoodProgrammer.jpg',
+    
+    }
+});
